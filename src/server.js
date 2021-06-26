748b8ec2d20ceac20e0635ea23c5b5695281b529
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-26 09:19:43 -0300

    :sparkles: update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
+// DEVELOPMENT ONLY require('dotenv').config();
 const express = require('express');
 const app = express();
 const routes = require('./routes');
-const port = 3000;
 const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +10,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
 app.use(routes);
 
-app.listen(port, () => {
-  console.log('App running at http://localhost:' + port)
+app.listen(process.env.PORT, () => {
+  console.log('Running');
 });
