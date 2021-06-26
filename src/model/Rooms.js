26e10356070bb18895f0ccf494113216bf83e12d
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-26 09:16:34 -0300

    :sparkles: update get questions model method

--- src/model/Rooms.js
+++ src/model/Rooms.js
@@ -1,6 +1,17 @@
 const startDb = require('../db/config');
 
 module.exports = {
+  async get(id) {
+    const db = await startDb();
+    const rooms = id
+      ? await db.get(`SELECT * FROM rooms WHERE id = ${id}`)
+      : await db.all('SELECT * FROM rooms');
+
+
+    await db.close();
+    return rooms;
+  },
+
   async create(roomId, roomPass) {
     const db = await startDb();
 
