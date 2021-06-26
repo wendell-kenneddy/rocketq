dd9abb04a1eeeee295e4f01c6e622cd88ef324d4
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-26 09:18:10 -0300

    :sparkles: update rooms controller

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,22 +1,69 @@
+const database = require('../db/config');
 const Rooms = require('../model/Rooms');
+const Questions = require('../model/Questions');
+
+// Checks if the generated id already exists
+const validateId = async id => {
+  const db = await database();
+  let existingIds = await db.all('SELECT id FROM rooms');
+  let idAlreadyExists = existingIds.some(roomId => roomId === id);
+  await db.close();
+  return idAlreadyExists;
+};
+
+const generateId = () => {
+  let roomId = Math.floor(Math.random() * 10).toString();
+
+  for (let i = 0; i < 5; i++) {
+    const randNum = Math.floor(Math.random() * 10).toString();
+
+    roomId += randNum;
+  }
+
+  return roomId;
+};
+
+// Recursive function that calls itself again until an valid id is created
+const tryCreateId = async () => {
+  const id = generateId();
+  const idAlreadyExists = await validateId(id);
+
+  if (idAlreadyExists) {
+    tryCreateId();
+    return;
+  }
+
+  return id;
+};
 
 module.exports = {
-  index(req, res) {
-    return res.render('room');
+  enter(req, res) {
+    const roomId = req.body['room-id'];
+    return res.redirect(`/room/${roomId}`);
   },
 
-  async createRoom(req, res) {
-    const roomPass = req.body.pass;
-    let roomId = Math.floor(Math.random() * 10).toString();
+  async open(req, res) {
+    const roomId = req.params.id;
+    const questions = await Questions.get(roomId);
+    const questionsRead = await Questions.get(roomId, true);
+    let noQuestions = questionsRead.length == 0 && questions.length == 0
+      ? true
+      : false;
 
-    for (let i = 0; i < 5; i++) {
-      const randNum = Math.floor(Math.random() * 10).toString();
+    return res.render('room', {
+      roomId,
+      questions,
+      questionsRead,
+      noQuestions
+    });
+  },
 
-      roomId += randNum;
-    }
+  async create(req, res) {
+    const roomPass = req.body['room-pass'];
+    const roomId = await tryCreateId();
 
     await Rooms.create(Number.parseInt(roomId), roomPass);
 
-    return res.redirect('/room/' + roomId);
+    return res.redirect(`/room/${roomId}`);
   }
 }
